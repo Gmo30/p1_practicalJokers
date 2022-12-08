4105beae50cb0b47d932f466944c620d768cf97f
Gordon Mo <gmo30@stuy.edu>
2022-12-08 14:05:10 -0500

    workin prt2

--- app/__init__.py
+++ app/__init__.py
@@ -5,26 +5,35 @@ Softdev P01
 time spent: 3 hours
 """
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__) 
 
 @app.route("/", methods=['GET', 'POST'])
 def disp_loginpage():
+    if(request.method == "GET"):
+        return render_template( 'login.html' ) #displays the login page
     if(request.method == "POST"):
         username = request.form['username']
         password = request.form['password']
-        print(username)
-        print(password)
-        return render_template( 'play.html' )#this page displays play page
-    if(request.method == "GET"):
-        return render_template( 'login.html' ) #displays the login page
+        return redirect(url_for('play'))#this page displays play page
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     if(request.method == "GET"):
         return render_template('register.html')  #displays register page
     else:
-        return render_template("play.html") #play will be the home page
+        username = request.form['username'].lower()
+        password = request.form['password']
+        password_confirm = request.form['password_confirm']
+        if(user_exists(username)):
+            return render_template('register.html', message = "User already exists")
+        else:
+            #if(password == password_confirm):
+            return redirect(url_for('login')) #when you register, redirects you to login
+
+#@app.route("/play")
+#def play():
+
 
 if __name__ == "__main__":  # true if this file NOT imported
     app.debug = True        # enable auto-reload upon code change
