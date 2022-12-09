0b351f2fab166cf002dd332bbfc6aa10af4430ee
AmeerA0506 <aalnasser30@stuy.edu>
2022-12-09 13:53:20 -0500

    db

--- app/db.py
+++ app/db.py
@@ -9,10 +9,23 @@ DB_FILE="back.db"
 db=sqlite3.connect(DB_FILE, check_same_thread=False)
 c=db.cursor()
 
-db.execute("CREATE TABLE if not exists consoomer(username text, password text, country text)")
+db.execute("CREATE TABLE if not exists consoomer(user text, password text, country text)")
 
-db.execute("CREATE TABLE if not exists dabloons(username text, highest real, current real, recent real)")
+db.execute("CREATE TABLE if not exists dabloons(user text, highest real, current real, recent real)")
 
 db.execute("CREATE TABLE if not exists country(country text, GDP real)")
 
 db.execute("Insert into country(USA,0)")
+
+db.execute("Insert into consoomer(?,?,?)", username, password, USA)
+
+def user_exists(username):
+    c=db.cursor()
+    result=c.execute("Select user from consoomer where username = ?", username)
+    try:
+        c.fetchone()[0]==username
+        c.close
+        return True
+    except: #If c.fetchone does not have an entry, then we want to catch the error and return an exception
+        c.close
+        return False
