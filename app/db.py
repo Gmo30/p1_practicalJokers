123ff66fe2ae15db449c48bbb44e6fe416fbad30
AmeerA0506 <aalnasser30@stuy.edu>
2022-12-15 14:05:51 -0500

    db work

--- app/db.py
+++ app/db.py
@@ -12,7 +12,7 @@ DB_FILE="back.db"
 db=sqlite3.connect(DB_FILE, check_same_thread=False)
 c = db.cursor()
 db.executescript("""
-CREATE TABLE if not exists consoomer(user text, password text, money int);
+CREATE TABLE if not exists consoomer(user text, password text, country text, money int);
 CREATE TABLE if not exists dabloons(user text, country text, highest real, current real, recent real);
 CREATE TABLE if not exists country(country text, GDP int);
 CREATE TABLE if not exists cards(deck_id text, total_value int)
@@ -34,9 +34,9 @@ def user_exists(username):
         c.close()
         return False
 
-def add_user(username, password):
+def add_user(username, password, country):
     c=db.cursor()
-    c.execute("Insert into consoomer values(?,?,?)", (username, password, "1000"))
+    c.execute("Insert into consoomer values(?,?,?)", (username, password, country, "1000"))
     c.close()
 
 def check_pass(username, password):
