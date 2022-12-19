c3154e950fb12871344136a7f0c62a19ebdfc41b
AmeerA0506 <aalnasser30@stuy.edu>
2022-12-19 14:06:30 -0500

    devlog

--- app/db.py
+++ app/db.py
@@ -15,7 +15,14 @@ db.executescript("""
 CREATE TABLE if not exists consoomer(user text, password text, country text, money int);
 CREATE TABLE if not exists dabloons(user text, country text, highest real, current real, recent real);
 CREATE TABLE if not exists country(country text, GDP int);
-CREATE TABLE if not exists cards(deck_id text, total_value int);
+CREATE TABLE if not exists dealercards(cardname text, cardname1 text, cardname2 text,cardname3 text, 
+    cardname4 text, cardname5 text, cardname6 text, cardname7 text, cardname8 text,
+    cardname9 text, cardname10 text, cardname11 text,  total_value int);
+CREATE TABLE if not exists playercards(cardname text, cardname1 text, cardname2 text,cardname3 text, 
+    cardname4 text, cardname5 text, cardname6 text, cardname7 text, cardname8 text,
+    cardname9 text, cardname10 text, cardname11 text,  total_value int);
+Insert into dealercard values('None','None','None','None','None','None','None','None','None','None','None','None',0);
+Insert into playercard values('None','None','None','None','None','None','None','None','None','None','None','None',0);
 Insert into consoomer values(?,?,?,?), ('aa', 'password', 'USA', '1000');
 """)
 c.close()
@@ -23,6 +30,10 @@ c.close()
 #c.execute("select * from consoomer")
 #print(c.fetchall())
 #c.close()
+#Select column1,column2, from dealercard/playercard
+#cursor fetchone
+# get tuple
+# for loop to check nones, then update cardnameN to card given
 
 def user_exists(username):
     c=db.cursor()
@@ -53,10 +64,7 @@ def check_pass(username, password):
 
 def update_country(user_new,country_new):
     c=db.cursor()
-    db.executescript("""
-    UPDATE consoomer
-    Set country = ?
-    where user=?""", country_new,user_new)
+    db.execute("UPDATE consoomer Set country = ? where user=?", country_new,user_new)
     c.close()
 
 def update_money_win(username, money_bet):
@@ -74,3 +82,27 @@ def update_money_lose(username, money_bet):
     after_bet = before_bet[0] - money_bet
     c.execute("UPDATE consoomer SET money = after_bet WHERE user =?", (username,))
     c.close()
+
+def add_player_card(card):
+    c=db.cursor()
+    c.execute()
+    c.close()
+def add_dealer(card):
+    c=db.cursor()
+    c.execute()
+    c.close()
+
+def reset_cards():
+    c=db.cursor()
+    c.executescript("""
+    CREATE TABLE 
+    dealercards(cardname text, cardname1 text, cardname2 text,cardname3 text, 
+    cardname4 text, cardname5 text, cardname6 text, cardname7 text, cardname8 text,
+    cardname9 text, cardname10 text, cardname11 text,  total_value int);
+    CREATE TABLE
+     playercards(cardname text, cardname1 text, cardname2 text,cardname3 text, 
+    cardname4 text, cardname5 text, cardname6 text, cardname7 text, cardname8 text,
+    cardname9 text, cardname10 text, cardname11 text,  total_value int);
+    Insert into dealercard values('None','None','None','None','None','None','None','None','None','None','None','None',0);
+    Insert into playercard values('None','None','None','None','None','None','None','None','None','None','None','None',0);
+    """)
